d4ce29f1e49e6cf1c56fdae678dd1aa518e4e0c9
s-serenity <476006557@qq.com>
2020-03-29 22:55:18 +0800

    Add insertion sort implementation

--- src/com/Sorts/SortTest.java
+++ src/com/Sorts/SortTest.java
@@ -1,7 +1,16 @@
 package com.Sorts;
 
+import com.Sorts.InsertionSort;
 
 public class SortTest {
     public static void main(String args[]){
+        int[] testList = {1,9,4,6,5,-4};
+        for (int x:testList){
+            System.out.print(x);
+        }
+        InsertionSort.insertionSort(testList);
+        for (int x:testList){
+            System.out.print(x);
+        }
     }
 }
